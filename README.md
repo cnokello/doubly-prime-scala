7ad708ca52a15ed1cfcc40d9cb3485db9882a1cd
unknown <Administrator@CRB155.crbafrica.com>
2014-11-29 19:49:27 +0300

    Algorithm modification and README

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ a prime number that also has  it's constituting digits  being prime.
 *What is the smallest prime number?*
 
 Original definitions of prime numbers included 1. However, recent definitions states that prime numbers begin at 2. 
-A look at [Wikipedia definition of prime numbers] (http://en.wikipedia.org/wiki/Prime_number) attest to this. Also, 
+A look at [Wikipedia definition of prime numbers] (http://en.wikipedia.org/wiki/Prime_number) attests to this. Also, 
 [a paper published in the Journal of Integer Sequences] (https://cs.uwaterloo.ca/journals/JIS/VOL15/Caldwell1/cald5.pdf) 
 tries to explain why the smallest prime number is not 1. 
 
@@ -30,7 +30,20 @@ However, instructions for this assignment adheres to the original definition of 
  
  *Prime number generation algorithm*
  
- The algorithm I've used for generating a list of N prime numbers is the [Sieve of Eratosthenes](http://rosettacode.org/wiki/Sieve_of_Eratosthenes). 
+ The algorithm I've used for generating a list of N prime numbers is the [Sieve of Eratosthenes](http://rosettacode.org/wiki/Sieve_of_Eratosthenes).
+ 
+ 
+ **Limitations of my Implementation**
+ 
+ * My implementation currently only uses a single core. Further work to enable it make use of several cores is one possible way to improve it's performance and total number of doubly prime numbers that it can generate
+ * There's a possibility that my implementation may not be able to generate infinite number of doubly prime numbers. It took 12 seconds to generate 10,000 doubly prime numbers on a 2.99GHz core. 
+ 	I've not worked out the complexity of my implementation of the algorithm. Vanilla sieve of eratosthenes algorithm has a time complexity of the order  O(n (log n) (log log n)) according to this [Wikipedia entry](http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes).    
+ 	
+ 	
+ **Some observations**
+ 
+ * Changing the first prime number from 1 to 2 has a huge effect on the total number of doubly prime numbers this implementation can generate and the time it takes to generate them.
+ * To generate 100 doubly prime numbers, it took about 40 milliseconds on 2.90GHz core. This is when other applications are also running.
  
  
  **How to run**
@@ -54,7 +67,7 @@ However, instructions for this assignment adheres to the original definition of 
 	
 	Sample output for N = 100, sample output is as shown below:
 		
-	> List(2, 3, 5, 7, 11, 13, 17, 23, 31, 37, 53, 71, 73, 113, 127, 131, 137, 151, 157, 173, 211, 223, 227, 233, 251, 257, 271, 277, 311, 313, 317, 331, 337, 353, 373, 521, 523, 525, 527, 531, 532, 533, 535, 537, 551, 552, 553, 555, 557, 571, 572, 573, 575, 577, 711, 712, 713, 715, 717, 721, 722, 723, 725, 727, 731, 732, 733, 735, 737, 751, 752, 753, 755, 757, 771, 772, 773, 775, 777, 1111, 1113, 1115, 1117, 1121, 1123, 1125, 1127, 1131, 1133, 1135, 1137, 1151, 1153, 1155, 1157, 1171, 1173, 1175, 1177, 1211)
+	> List(1, 2, 3, 5, 7, 11, 13, 17, 23, 31, 37, 53, 71, 73, 113, 127, 131, 137, 151, 157, 173, 211, 223, 227, 233, 251, 257, 271, 277, 311, 313, 317, 331, 337, 353, 373, 521, 523, 551, 557, 571, 577, 727, 733, 751, 757, 773, 1117, 1121, 1123, 1151, 1153, 1171, 1213, 1217, 1223, 1231, 1237, 1273, 1277, 1321, 1327, 1373, 1511, 1523, 1531, 1537, 1553, 1571, 1577, 1711, 1721, 1723, 1733, 1753, 1777, 2111, 2113, 2117, 2131, 2137, 2153, 2173, 2213, 2221, 2237, 2251, 2273, 2311, 2333, 2351, 2357, 2371, 2377, 2521, 2531, 2537, 2551, 2557, 2711)
 	 
 		
 
--- src/main/scala/intervw/DoublyPrimeNumbers.scala
+++ src/main/scala/intervw/DoublyPrimeNumbers.scala
@@ -1,84 +1,68 @@
 package intervw
 
+case class Cross(next: Int, incr: Int)
+
 /**
  * @author nelson.okello
  *
  * Generates doubly prime numbers.
  * It uses the  Sieve of Erastothenes algorithm to generate a sequence of N prime numbers
  *
- * For the generated prime numbers,
- * it checks whether the constituent digits of each prime number is also a prime number. If any of the constituent digits
- * of any generated prime number is not a prime number, the corresponding prime number is removed from the list.
- * This process is applied recursively until all elements which are not doubly prime are removed.
- *
- *  If after removing non-doubly prime numbers, the remaining doubly prime number is less than the requested number, N,
- *  recursively call get primes
- *
  */
-object DoublyPrimeNumbers {
+class DoublyPrimeNumbersGenerator(N: Int) {
   import scala.collection.mutable.ListBuffer
 
-  var N: Int = 0
-  var doublyPrimes: ListBuffer[Int] = ListBuffer[Int]()
-
   /**
-   * Returns n doubly prime numbers
+   * Returns N doubly prime numbers
    */
-  def get(n: Int): List[Int] = {
+  def get: List[Int] = {
 
     /**
-     * Generates prime numbers
+     * Eratosthenes algorithm for N prime numbers generation
+     * I've adjusted this algorithm a little bit to take care of the doubly prime number condition
+     * of ensuring that every constituent digit is a prime number as well
      */
-    def sieve(s: Stream[Int]): Stream[Int] = {
-      s.head #:: sieve(s.tail.filter(_ % s.head != 0))
+    def sieve(s: Stream[Int], crosses: List[Cross]): Stream[Int] = {
+      val current #:: rest = s
+
+      if (notPrime(crosses, current)) sieve(rest, adjustCrosses(crosses, current))
+      else current #:: sieve(rest, Cross(current * current, current) :: crosses)
+    }
+
+    def adjustCrosses(crosses: List[Cross], current: Int) = {
+      crosses map {
+        case cross @ Cross(`current`, incr) => cross copy (next = current + incr)
+        case unchangedCross => unchangedCross
+      }
     }
 
     /**
-     * Uses a custom algorithm to generate doubly prime numbers
-     * Generates 100 doubly prime number in about  150ms on average using a single core of 2.90 GHz
-     * The algorithm used to generate doubly prime numbers is a greedy algorithm
-     * On my PC, it can comfortably generate 160 doubly prime numbers with no custom JVM configuration
-     * To generate more than 160 doubly prime numbers requires additional work on the algorithm
+     * Modification of the Eratosthenes algorithm to take care of the double prime number
+     * condition is implemented here
      */
-    def getPrimes(lower: Int, numPrimes: Int): List[Int] = {
+    def notPrime(crosses: List[Cross], current: Int): Boolean = {
+      val prime = crosses exists (_.next == current)
 
-      /**
-       * Returns the largest integer number in the specified list
-       */
-      def max(xs: ListBuffer[Int]) = {
-        if (xs.isEmpty) throw new NoSuchElementException
-        xs.reduceLeft((x, y) => if (x > y) x else y)
+      current.toString.map(_.asDigit).toList.foreach { n =>
+        if (!isPrime(n)) return true
       }
 
-      /**
-       * Returns TRUE if n is a prime number. FALSE otherwise.
-       */
-      def isPrime(n: Int): Boolean = {
-        if (n < 1) false
-        else if (n == 1 || n == 2) true
-        else !(2 to (n - 1)).exists(x => n % x == 0)
-      }
-
-      if (N == 0) N = numPrimes
-      var primes = sieve(Stream.from(lower))
-      var _primes = primes.take(numPrimes).to[ListBuffer]
-      _primes.foreach { prime =>
-        prime.toString.map(_.asDigit).toList.foreach { n =>
-          if (!isPrime(n)) {
-            _primes = _primes diff List(prime)
-          }
-        }
-      }
-
-      doublyPrimes ++= _primes
-      if (doublyPrimes.size >= N) doublyPrimes.take(N).toList
-      else if (N <= 7) getPrimes(max(_primes) + 1, (N - doublyPrimes.size) * N)
-      else if (N <= 100) getPrimes(max(_primes) + 1, (N - doublyPrimes.size) * (N / 6))
-      else getPrimes(max(_primes) + 1, (N - doublyPrimes.size) * (N / 5))
+      return prime
     }
 
-    getPrimes(2, n)
+    /**
+     * Returns TRUE if n is a prime number. FALSE otherwise.
+     */
+    def isPrime(n: Int): Boolean = {
+      if (n < 1) false
+      else if (n == 1 || n == 2) true
+      else !(2 to (n - 1)).exists(x => n % x == 0)
+    }
+
+    val primes = sieve(Stream.from(1), Nil)
+    primes.take(N).toList
   }
+
 }
 
 /**
@@ -87,15 +71,17 @@ object DoublyPrimeNumbers {
 object RunApp extends App {
   override def main(args: Array[String]) = {
     val start = System.nanoTime()
-    var doublyPrimes = List[Int]()
+    var N = 100
 
-    if (args.size == 0) doublyPrimes = DoublyPrimeNumbers.get(100)
-    else try doublyPrimes = DoublyPrimeNumbers.get(args(0).toInt) catch {
-      case e: Exception => println("Invalid argument provided!")
-    }
+    if (args.size > 0)
+      try N = args(0).toInt catch {
+        case e: Exception => println("Invalid argument provided!")
+      }
+
+    val doublyPrimeNumbers = new DoublyPrimeNumbersGenerator(N).get
 
     println("Time taken to compute: " + (System.nanoTime() - start) / 1000000 + " milliseconds")
-    println("Total doubly prime numbers generated: " + doublyPrimes.size)
-    println(doublyPrimes)
+    println("Total doubly prime numbers generated: " + doublyPrimeNumbers.size)
+    println(doublyPrimeNumbers)
   }
 }
